60e6fddf40f2adcbe3d8c93e40c36a188ab4a0d7
Will <doggy.huang@gmail.com>
2018-07-21 00:36:07 +0800

    將 forms.html 轉換成 FormsComponent 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavComponent } from './nav/nav.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavComponent
+    NavComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
